87f42a28ee4c3433f810cbf9e209e667fb1cd7a4
Micah Johnston <micah@glowcoil.com>
2018-12-21 22:38:50 -0600

    templates and styling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[macro_use]
 extern crate rouille;
 #[macro_use]
 extern crate tera;
@@ -16,17 +17,36 @@ fn main() {
     let db = Mutex::new(Connection::open(Path::new("parnassum.db")).unwrap());
 
     rouille::start_server("0.0.0.0:80", move |request| {
-        let db = db.lock().unwrap();
-
-        let mut stmt = db.prepare("SELECT name FROM users").unwrap();
-        let mut rows = stmt.query_map(NO_PARAMS, |row| row.get(0)).unwrap();
-        let mut values: Vec<String> = Vec::new();
-        for row in rows {
-            values.push(row.unwrap());
+        /* serve static files from static/ */
+        if let Some(request) = request.remove_prefix("/static") {
+            let response = rouille::match_assets(&request, "static");
+            if response.is_success() {
+                return response;
+            }
         }
 
-        let mut context = Context::new();
-        context.insert("a", &values);
-        Response::html(tera.render("index.html", &context).unwrap())
+        router! { request,
+            (GET) (/) => {
+                let db = db.lock().unwrap();
+
+                let mut stmt = db.prepare("SELECT name FROM users").unwrap();
+                let mut rows = stmt.query_map(NO_PARAMS, |row| row.get(0)).unwrap();
+                let mut values: Vec<String> = Vec::new();
+                for row in rows {
+                    values.push(row.unwrap());
+                }
+
+                let mut context = Context::new();
+                context.insert("a", &values);
+                Response::html(tera.render("index.html", &context).unwrap())
+            },
+
+            _ => {
+                let mut context = Context::new();
+                let mut response = Response::html(tera.render("404.html", &context).unwrap());
+                response.status_code = 404;
+                response
+            },
+        }
     });
 }
