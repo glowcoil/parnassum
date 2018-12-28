0b567b4f79612409a16d903e63d01e18dcd3e57b
Micah Johnston <micah@glowcoil.com>
2018-12-28 16:16:22 -0600

    commit schema

---
+++ src/sql/schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE "users" (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `name` TEXT NOT NULL UNIQUE,
+    `password` TEXT NOT NULL,
+    `salt` TEXT NOT NULL,
+    `created` TEXT
+);
+
+CREATE TABLE "worklogs" (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `user_id` INTEGER NOT NULL,
+    `text` TEXT NOT NULL,
+    `link` TEXT,
+    `created` TEXT
+);
+
+CREATE TABLE "sessions" (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `user_id` INTEGER NOT NULL,
+    `token` TEXT NOT NULL,
+    `created` TEXT
+);
+
+CREATE UNIQUE INDEX `sessions_token` ON `sessions` ( `token` ASC );
